a2ab58d12f4ad9e65d75246b34a04057389011ea
Jovan Mwesigwa <62109301+JovanMwesigwa@users.noreply.github.com>
2024-01-22 14:05:45 +0300

    Update siwe.js
    Dynamically create the SIWE

--- siwe.js
+++ siwe.js
@@ -1,20 +1,22 @@
-import { SiweMessage } from "siwe";
+import { SiweMessage, generateNonce } from "siwe";
 
 async function createSiweMessage() {
   try {
     const currentDate = new Date();
     const expirationDate = new Date(currentDate.getTime() + 60 * 60 * 3000); // Add 1 hour in milliseconds
 
+    const nonce = generateNonce();
+
     const formattedExpirationDate = expirationDate.toISOString();
 
     const message = new SiweMessage({
       statement: "Sign in with Ethereum to the app.",
-      address: "", // Enter your address here
+      address: "0x5CBDf5f9E468dF3888e04155668CcAfC6F6C4dcf",
       domain: "sandbox.oneramp.io",
-      uri: "https://sandbox.oneramp.io",
+      uri: "https://sandbox.oneramp.io/auth/login",
       version: "1",
       chainId: "44787",
-      nonce: "", // Generate a random nonce using command `openssl rand -hex 16`
+      nonce: nonce, // Generate and update random nonce using openssl rand -hex 16
       issuedAt: currentDate.toISOString(),
       expirationTime: formattedExpirationDate,
     });
